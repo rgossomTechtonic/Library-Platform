7d17d4d6a4e65c6e3a03af10d1d3b8b28671bef1
Romero Gossom <>
2018-10-02 15:02:32 -0600

    Updated file

--- book.js
+++ book.js
@@ -4,7 +4,9 @@ function Book(title, author, numPages, pubDate)
   this.author = author;
   this.numPages = numPages;
   this.pubDate = pubDate;
+
 };
+
 
 Book.prototype.editBook = function(oBook)
 {
