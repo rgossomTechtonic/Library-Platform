01e5088d4ea3883e3e9ad2e2d591bbec3c9664f2
Romero Gossom <>
2018-10-04 13:39:28 -0600

    I've made the Author & the Book title.

--- js/engine.js
+++ js/engine.js
@@ -1,7 +1,7 @@
 function Library(){
  this.bookShelf = new Array();
 }
-
+                                       //Book with all values gives me the whole book so use it
 Library.prototype.addBook = function(book){
   //check if book is already in my array (this.bookShelf)
   //Add book thats coming from book param if the title is not already in arr
@@ -20,29 +20,56 @@ Library.prototype.addBook = function(book){
   return true;
   //console.log(true);
 };
-
-Library.prototype.removeBookByTitle = function(title)
-{
-// I have to remove a book from the book array by its title
+                                                //This give me the title so use it (just the title).
+Library.prototype.removeBookByTitle = function(title){
+// I have to remove a book from the book array by it's title find books
 //return true if the book was removed * false if no book matched
 
- this.BookShelf.splice(title);
+// This was put here for me to loop through
 
+  for(var index=0; index<this.bookShelf.length; index++) {
+   if(this.bookShelf[index].title === title){
+     this.bookShelf.splice(index,1);
+    return true;
+    //console.log(false);
+  }
+}
+return false;
+//console.log(true);
 };
 
+                                                //This give me the author so use it and it should work fine for me.
+Library.prototype.removeBookByAuthor = function(author){
+
+for(var index = 1; index<this.bookShelf.length; index++) {
+  if(this.bookShelf[index].author === author){
+    this.bookShelf.splice(index,2);
+      return true;
+      //console.log(true);
+    }
+  }
+  return false;
+  //console.log(false);
+};
+
+
+Library.prototype.getRandomBook = function(book){
+
+
+
+this.bookShelf.math.Random.floor();
+};
+
+//This is my EventListener this how the Dom listens for me to basically ask for something.
 document.addEventListener("DOMContentLoaded", function(e){
   window.gLibrary = new Library();
 
-
-});
-
-
-// I put this here so it will show up in the console. The set timeout just lets the runtime have a delay in the console
-
-setTimeout(function(){
   gLibrary.addBook(jay);
   gLibrary.addBook(iOprah);
-},500);
+  gLibrary.addBook(lol);
+  gLibrary.removeBookByAuthor();
+});
+
 
  // This code with my book should be added in addEventListener that way I can test it.
 // window.book1 = new Book("it", "sk", 200, "jan 1");
